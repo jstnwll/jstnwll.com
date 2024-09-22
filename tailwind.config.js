5c4f154297de507729d9fc6b0522da8376448e69
jstnwll <125165923+jstnwll@users.noreply.github.com>
2024-09-22 08:55:50 -0800

    Website update

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,11 @@ module.exports = {
     extend: {
       fontFamily: {
         marker: ['"Permanent Marker"', "sans-serif"],
+        raleway: ['"Raleway"', "sans-serif"],
+        exo: ['"Exo"', "sans-serif"],
+        montserrat: ['"Montserrat"', "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
